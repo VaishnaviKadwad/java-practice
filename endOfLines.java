85396b5aac87501c32b0bcfdf4c03dcda42fb894
VaishnaviKadwad <vaishnavikadwad1@gmail.com>
2026-07-21 19:58:49 +0530

    Add exit when end of line

---
+++ endOfLines.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class endOfLines {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int lineNumber = 1;
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            System.out.println(lineNumber + " " + line);
+            lineNumber++;
+        }
+
+        sc.close();
+    }
+}
